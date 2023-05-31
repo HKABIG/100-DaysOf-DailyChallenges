e5ca127dfe8250847563897e26b056a1e7c47d15
gonap <ajaygonepuri@gmail.com>
2023-05-31 12:41:51 +0530

    Day-7

---
+++ GFG/Frequency Game.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int LargButMinFreq(int arr[], int n) {
+        map<int,int> mp;
+        for(int i=0;i<n;i++){
+        	mp[arr[i]]++;
+        }
+        int mini = INT_MAX;
+        int ans = INT_MIN;
+        for(auto it:mp){
+        	mini = min(it.second,mini);
+        }
+        for(auto it:mp){
+        	if(it.second == mini){
+        		ans = max(ans,it.first);
+        	}
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    // Iterating over testcases
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int arr[n];
+
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution ob;
+
+        cout << ob.LargButMinFreq(arr, n) << endl;
+    }
+}
+// } Driver Code Ends
