7e5bd3ea84e12366e90a7d1ac4cdbc7fa8008f20
gonap <ajaygonepuri@gmail.com>
2023-06-27 21:32:38 +0530

    Day-35

---
+++ GFG/Union of Two Linked Lists.cpp
@@ -0,0 +1,112 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+
+// } Driver Code Ends
+/*
+// structure of the node is as follows
+
+struct Node
+{
+	int data;
+	struct Node* next;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	}
+	
+};
+
+*/
+class Solution
+{
+    public:
+    struct Node* makeUnion(struct Node* head1, struct Node* head2)
+    {
+        set<int> set;
+        Node* t1 = head1;
+        while(t1!=NULL) {
+            set.insert(t1->data);
+            t1 = t1->next;
+        }
+        Node* t2 = head2;
+        while(t2!=NULL){
+            set.insert(t2->data); 
+            t2 = t2->next;
+        } 
+        Node* nHead = new Node(0);
+        Node* temp = nHead;
+        for(auto it:set){
+            temp->next = new Node(it);
+            temp = temp->next;
+        }
+        return nHead->next;
+    }
+};
+
+//{ Driver Code Starts.
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        Solution obj;
+        Node* head = obj.makeUnion(first,second);
+        printList(head);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
---
+++ LeetCode/373. Find K Pairs with Smallest Sums.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<vector<int>> ans;
+        set<pair<int,int>> vis;
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
+                       greater<pair<int, pair<int, int>>>> minHeap;
+        minHeap.push({nums1[0]+nums2[0],{0,0}});
+        vis.insert({0,0});
+        while(k-- && !minHeap.empty()){
+            auto top = minHeap.top();
+            minHeap.pop();
+            int i = top.second.first;
+            int j = top.second.second;
+            ans.push_back({nums1[i],nums2[j]});
+            if(i+1<m && vis.find({i+1,j})==vis.end()){
+                minHeap.push({nums1[i+1]+nums2[j],{i+1,j}});
+                vis.insert({i+1,j});
+            }
+            if(j+1<n && vis.find({i,j+1})==vis.end()){
+                minHeap.push({nums1[i]+nums2[j+1],{i,j+1}});
+                vis.insert({i,j+1});
+            }
+        }
+        return ans;
+    }
+};
