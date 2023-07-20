123e2676fff191aaf27675a75912c679c0fbd0d0
Ajay Gonepuri <ajaygonepuri@gmail.com>
2023-07-20 22:07:02 +0530

    Day-58

---
+++ LeetCode/735. Asteroid Collision.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        stack<int> st;
+        for(int i=0;i<asteroids.size();i++){
+            if(st.empty() || asteroids[i]>0){
+                st.push(asteroids[i]);
+            }
+            else{
+                while(!st.empty() && st.top()>0 && st.top()<abs(asteroids[i])){
+                    st.pop();
+                }
+                if(!st.empty() && st.top()==abs(asteroids[i])){
+                    st.pop();
+                }
+                else{
+                    if(st.empty() || st.top()<0){
+                        st.push(asteroids[i]);
+                    }
+                }
+            }
+        }
+        vector<int> res(st.size(),0);
+        for(int i=(int)st.size()-1;i>=0;i--){
+            res[i] = st.top();
+            st.pop();
+        }
+        return res;
+    }
+};
